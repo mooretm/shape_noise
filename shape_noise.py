fe2c671e37a3c0b913d461a829a38e3b6fc91dc7
Travis Moore <travis_moore@starkey.com>
2022-06-17 12:27:52 -0500

    Cleaned up code. Removed FFT (just P Welch remains).

--- shape_noise.py
+++ shape_noise.py
@@ -17,29 +17,10 @@ import importlib
 importlib.reload(ts) # Reload custom module on every run
 
 
-def butter_lowpass(cutoff, fs, order=5):
-    nyq = 0.5 * fs
-    normal_cutoff = cutoff / nyq
-    b, a = signal.butter(order, normal_cutoff, btype='low', analog=False)
-    return b, a
-
-def butter_lowpass_filter(data, cutoff, fs, order=5):
-    b, a = butter_lowpass(cutoff, fs, order=order)
-    #y = signal.lfilter(b, a, data)
-    y = signal.filtfilt(b, a, data)
-    return y
-
-def butter_filt(sig, type, cutoff, order, fs=48000):
-    if type == 'low' or type == 'high':
-        nyq = 0.5 * fs
-        norm_cutoff = cutoff / nyq
-        b, a = signal.butter(order, norm_cutoff, btype=type, analog=False)
-        y = signal.filtfilt(b, a, sig)
-        return y
-
 def filter_delay(num_taps, fs):
     filt_delay = (num_taps - 1) / (2 * fs)
     return filt_delay
+
 
 def filter_taps(d1=10**-4, d2=10**-3, Df=1000):
     """ https://dsp.stackexchange.com/questions/31066/how-many-taps-does-an-fir-filter-need
@@ -50,10 +31,10 @@ def filter_taps(d1=10**-4, d2=10**-3, Df=1000):
     return num_taps
 
 
-
 # Ensure that relative paths start from the same directory as this script
 _thisDir = os.path.dirname(os.path.abspath(__file__))
 os.chdir(_thisDir)
+
 
 def import_stim_file():
     """ Select file using system file dialog 
@@ -64,14 +45,9 @@ def import_stim_file():
     return fs, stimulus, file_name
 
 
-def mk_wgn(fs):
+def mk_wgn(fs,dur):
     """ Function to generate white Gaussian noise.
     """
-    dur=3
-    #lvl = ts.rms(sig)
-    # Set random seed
-    # This ensures the same random values are used to 
-    # generate the noise every time
     random.seed(4)
     wgn = [random.gauss(0.0, 1.0) for i in range(fs*dur)]
     wgn = ts.doNormalize(wgn)
@@ -79,57 +55,44 @@ def mk_wgn(fs):
     return wgn
 
 
+######################
+# Initialize Signals #
+######################
 # Read in signal from file
 fs, stimulus, file_name = import_stim_file()
 dur_stim = len(stimulus)/ fs
-
-# Calculate time base
 t_stim = np.arange(0,dur_stim,1/fs)
-
-# Normalize stimulus and noise
 stimulus = ts.doNormalize(stimulus)
-noise = mk_wgn(fs)
+
+# Make noise
+noise = mk_wgn(fs,3)
 dur_noise = len(noise) / fs
-
-stimulus[:len(noise)]
-
 t_noise = np.arange(0,dur_noise,1/fs)
-
-# FFT of stimulus and noise
-freqs_stimulus, fft_stimulus = ts.doFFT(stimulus,fs)
-freqs_noise, fft_noise = ts.doFFT(noise,fs)
 
 # P Welch of noise and stimulus tones
 f_noise, den_noise = signal.welch(noise, fs, nperseg=2048)
-f_stimulus, den_stimulus = signal.welch(stimulus, fs, nperseg=2048)
-
+f_stim, den_stim = signal.welch(stimulus, fs, nperseg=2048)
 
 # Plot unprocessed signal data
-fig1, axs1 = plt.subplots(nrows=2,ncols=3)
+fig1, axs1 = plt.subplots(nrows=2,ncols=2)
 # Normalized stimulus in the time domain
 axs1[0,0].plot(t_stim,stimulus)
-axs1[0,0].set_title('Original stimulus in time')
-# FFT of stimulus
-axs1[0,1].plot(freqs_stimulus,fft_stimulus)
-axs1[0,1].set_title('FFT Stimulus')
+axs1[0,0].set_title('Stimulus Waveform')
 # P Welch stimulus
-axs1[0,2].plot(f_stimulus,den_stimulus)
-axs1[0,2].set_title('P Welch stimulus')
+axs1[0,1].plot(f_stim, np.sqrt(den_stim))
+axs1[0,1].set_title('Spectral Density of Stimulus')
 # Normalized noise in the time domain
 axs1[1,0].plot(t_noise,noise)
-axs1[1,0].set_title('Original noise in time')
-# FFT noise
-axs1[1,1].plot(freqs_noise,fft_noise)
-axs1[1,1].set_title('FFT Noise')
+axs1[1,0].set_title('Noise Waveform')
 # P Welch noise
-axs1[1,2].semilogy(f_noise, den_noise)
-axs1[1,2].set_title('P Welch Noise')
+axs1[1,1].semilogy(f_noise, np.sqrt(den_noise))
+axs1[1,1].set_title('Spectral Density of Noise')
 plt.show()
 
 
-#########################################
-# Create FIR with shape of stimulus FFT #
-#########################################
+#################################################
+# Create FIR from spectral density of stimulus  #
+#################################################
 # Set number of filter taps
 num_taps = filter_taps()
 print(f"Number of taps: {num_taps}")
@@ -143,12 +106,9 @@ offset = num_taps - 1
 filt_delay = filter_delay(num_taps,fs)
 #print(f"Filter delay (s): {filt_delay}")
 
-f_stim, den_stim = signal.welch(stimulus, fs, nperseg=2048)
-
 # Create the filter
-#fir_filt = signal.firwin2(numtaps=num_taps, freq=freqs_stimulus/np.max(freqs_stimulus), gain=fft_stimulus)
 fir_filt = signal.firwin2(numtaps=num_taps, freq=f_stim/np.max(f_stim), gain=np.sqrt(den_stim))
-# Examine FIR frequency response
+# FIR frequency response
 w, h = signal.freqz(fir_filt)
 w = w * fs / (2*np.pi)
 # Apply FIR to noise
@@ -157,18 +117,10 @@ filtered_noise = np.convolve(fir_filt, noise)
 filtered_noise = filtered_noise / np.max(np.abs(filtered_noise))
 # Remove the extra values added during convolution
 filtered_noise = filtered_noise[:-offset]
+# P Welch of filtered noise
+f_filt_noise, den_filt_noise = signal.welch(filtered_noise, fs, nperseg=2048)
 
-
-#########################
-# FFT of filtered noise #
-#########################
-#freqs_filtered_noise, fft_filtered_noise = ts.doFFT(filtered_noise[:-offset],fs)
-freqs_filtered_noise, fft_filtered_noise = ts.doFFT(filtered_noise,fs)
-
-
-#######################
-# Plot filtering data #
-#######################
+# Plot filter data
 fig2, axs2 = plt.subplots(nrows=2, ncols=3)
 # FIR filter shape
 axs2[0,0].plot(fir_filt)
@@ -180,21 +132,20 @@ axs2[0,1].set_ylabel("Amplitude (dB)")
 axs2[0,1].set_title("FIR frequency response")
 # filtered noise
 axs2[0,2].plot(t_noise,filtered_noise)
-axs2[0,2].set_title('Filtered noise')
-# Original FFT of stimulus
-axs2[1,0].plot(freqs_stimulus,fft_stimulus)
-axs2[1,0].set_title('Original FFT of stimulus')
-# FFT of filtered noise
-axs2[1,1].plot(freqs_filtered_noise, fft_filtered_noise)
-axs2[1,1].set_title('FFT of filtered noise')
+axs2[0,2].set_title('Filtered Noise')
+# Original P Welch of stimulus
+axs2[1,0].plot(f_stim,den_stim)
+axs2[1,0].set_title('Original Spectral Density of Stimulus')
+# P Welch of filtered noise
+axs2[1,1].plot(f_filt_noise, den_filt_noise)
+axs2[1,1].set_title('Spectral Density of Filtered Noise')
 # Show Fig2
 plt.show()
 
 
-
-
-
-
+########################
+# Amplitude Correction #
+########################
 rms_stim = ts.rms(stimulus)
 filtered_noise = ts.doGate(sig=filtered_noise,rampdur=0.01,fs=fs)
 filtered_noise = ts.doNormalize(filtered_noise)
@@ -204,43 +155,34 @@ print(f"RMS of stimulus: {rms_stim}")
 print(f"RMS of filtered noise: {rms_filt_noise}")
 print(f"RMS diff: {amp_diff}")
 adj_filtered_noise = filtered_noise * amp_diff
-freqs_adj_filtered_noise, fft_adj_filtered_noise = ts.doFFT(adj_filtered_noise,fs)
+f_adj_filt_noise, den_adj_filt_noise = signal.welch(adj_filtered_noise, fs, nperseg=2048)
 print(f"RMS of adjusted filtered noise: {ts.rms(adj_filtered_noise)}")
 
 
-
-
-f_noise, den_noise = signal.welch(adj_filtered_noise, fs, nperseg=2048)
-f_stim, den_stim = signal.welch(stimulus, fs, nperseg=2048)
 #####################
 # Visual comparison #
 #####################
-#FFT plots
+# Spectral density plots
 plt.subplot(2,1,1)
 plt.plot(f_stim,20*np.log10(den_stim),color="blue",label="stimulus")
-plt.plot(f_noise,20*np.log10(den_noise),":",color="orange",label="Filtered Noise")
-#plt.plot(freqs_stimulus,fft_stimulus,color="blue",label="stimulus")
-#plt.plot(freqs_adj_filtered_noise,fft_adj_filtered_noise,":",color="orange",label="Filtered Noise")
-plt.title("Spectra after amplitude correction")
+plt.plot(f_adj_filt_noise,20*np.log10(den_adj_filt_noise),ls=":",color="orange",label="Filtered Noise")
+plt.title("Spectra after Amplitude Correction")
 plt.legend()
 
 # Waveform plots
-# Plot before/after amplitude correction
 dur_noise = len(adj_filtered_noise) / fs
 t_noise = np.arange(0,dur_noise,1/fs)
 plt.subplot(2,1,2)
 plt.plot(t_stim,stimulus,color="blue",label="stimulus")
 plt.plot(t_noise,adj_filtered_noise,color="orange",ls=":",label="Filtered Noise")
-#plt.plot(freqs_stimulus,env_stimulus,color="blue",label="stimulus")
-#plt.plot(freqs_filtered_noise,env_filtered_noise,color="orange",label="Filtered Noise")
-plt.title("Waveforms after amplitude correction")
+plt.title("Waveforms after Amplitude Correction")
 plt.legend()
 plt.show()
 
 
-
-
+##########
+# Output #
+##########
 # Write filtered noise to file
-
 file_out = file_name[:-4] + "_calibration.wav"
 wavfile.write(file_out, fs, adj_filtered_noise)
